509438ae7ac40ab45dc28c75979d8000e15eca42
Joe <a840409g@gmail.com>
2019-03-07 18:55:18 +0800

    簡易匯率計算機

---
+++ Computer.js
@@ -0,0 +1,55 @@
+const cur = new Vue({
+	el: '#cur',
+	data:{
+		NT: "",
+		countrys:[
+			{
+				country: "US",
+				rate: 31.135
+			},
+			{
+				country: "JP",
+				rate: 0.2796
+			},
+			{
+				country: "HK",
+				rate: 3.981
+			},
+			{
+				country: "EU",
+				rate: 35.42
+			},
+			{
+				country: "KR",
+				rate: 0.02951
+			},
+		]
+	},
+	computed:{
+		// calu: function(){
+		// 	for(let i=0;i<this.countrys.length;i++){
+		// 		if(this.countrys[0].country == "US"){
+		// 			return this.NT * this.countrys[0].rate
+		// 		}
+		// 	}
+		// },
+		US: function(){
+			return this.NT / 31.135
+		},
+		JP: function(){
+			return this.NT / 0.2796
+		},
+		HK: function(){
+			return this.NT / 3.981
+		},
+		EU: function(){
+			return this.NT / 35.42
+		},
+		KR: function(){
+			return this.NT / 0.02951
+		}
+	}
+})
+// const BMI = new Vue({
+	
+// })
